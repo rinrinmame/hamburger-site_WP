f2bc22fe900a1aa05b95c77c8b8b50c74259965d
rinrinmame <rina143606@gmail.com>
2024-08-31 00:40:24 +0900

    8/31 Googleマップ埋め込み、スタイリング

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
             <article class="l-main__contents p-title">
                 <?php 
                     $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-                    $noimage = get_template_directory_uri() . './images/noimage.jpg';
+                    $noimage = get_template_directory_uri() . '../images/noimage.jpg';
                 ?>
                 <?php if ($url) : ?>
                     <div class="c-background__title" style="background-image: url( '<?php echo $url; ?>' );">
@@ -59,17 +59,17 @@
                     </div>
                 </div>
             </article>
-            <section class="1-main__access p-access">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.7477988418323!2d139.74285797413486!3d35.65858483122038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bbd9009ec09%3A0x481a93f0d2a409dd!2z5p2x5Lqs44K_44Ov44O8!5e0!3m2!1sja!2sjp!4v1724943929863!5m2!1sja!2sjp?wmode=transparent" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" wmode="Opaque"></iframe>
-                <div class="c-background__access">
-                    <div class="p-access__map c-background__map">
-                        <div class="p-access__comment">
+            <section class="l-main__access p-access">
+                <div class="p-access__map">
+                    <div class="c-background__map-all">
+                        <div class="p-access__comment c-background__map-comment">
                             <div class="c-inner__text">
                                 <h2>見出しが入ります</h2>
                                 <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>   
                             </div>
                         </div>
                     </div>
+                    <iframe class="c-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.7477988418323!2d139.74285797413486!3d35.65858483122038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bbd9009ec09%3A0x481a93f0d2a409dd!2z5p2x5Lqs44K_44Ov44O8!5e0!3m2!1sja!2sjp!4v1724943929863!5m2!1sja!2sjp?wmode=transparent" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" wmode="Opaque"></iframe>
                 </div>
             </section>
         </main>
