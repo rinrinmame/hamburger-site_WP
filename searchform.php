b480536fe10ca1f61bf3f1ac8a323e7ea210ed36
rinrinmame <rina143606@gmail.com>
2024-10-19 09:17:21 +0900

    10/19 searchform.phpリファクタリング

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form class="c-flexbox__search-form" method="get" action="<?php echo esc_url( home_url('/') ); ?>">
     <label class="screen-reader-text" for="s">検索:</label>
-    <input class="c-search-form__keyword"  name="s" id="s" type="text" placeholder="キーワードを入力"/>
-    <input class="c-search-form__submit" type="submit" value="検索">
+    <input class="c-search-form__keyword"  name="s" id="s" type="text" placeholder="キーワードを入力" />
+    <input class="c-search-form__submit" type="submit" value="検索" />
 </form>
