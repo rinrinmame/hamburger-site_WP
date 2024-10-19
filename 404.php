ad0b6c329b7c97a47025612420f0457bd9408320
rinrinmame <rina143606@gmail.com>
2024-10-19 22:31:46 +0900

    10/19 404.php class名修正

--- 404.php
+++ 404.php
@@ -1,28 +1,26 @@
 <?php get_header(); //header.phpを読み込むテンプレートタグ（インクルードタグ）?>
-        <main class="l-main">
-            <article class="l-main__title p-title">
-                <div class="c-background__cheeseburger">
-                    <div class="c-background__cheeseburger-cover">
-                        <ul class="c-flexbox__title">
-                            <li class="c-title__archive"><h2>404 Not Found</h2></li>
-                        </ul>
+<main class="l-main c-grid__body__child--main">
+    <article class="l-main__title p-title">
+        <div class="c-background__archive">
+            <div class="c-background__archive-cover">
+                <ul class="c-flexbox__title">
+                    <li class="c-title__archive"><h2>404 Not Found</h2></li>
+                </ul>
+            </div>
+        </div>
+    </article>
+    <section class="l-main__discription p-discription__404">
+        <div class="c-inner__archive">
+            <div class="c-background__discription">
+                <div class="c-inner__discription">
+                    <div class="c-flexbox__discription">
+                        <h2 class="c-title__discription">お探しのページは見つかりませんでした</h2>
+                        <p>このページは削除されたかURLが変更されたため、表示することができません。</p>
                     </div>
                 </div>
-            </article>
-            <section class="l-main__discription p-discription">
-                <div class="c-inner__archive">
-                    <div class="c-background__discription">
-                        <div class="c-inner__discription">
-                            <div class="c-flexbox__discription">
-                                <h2 class="c-title__discription">お探しのページは見つかりませんでした</h2>
-                                <p>このページは削除されたかURLが変更されたため、表示することができません。</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </main>
-        <?php get_sidebar(); //siderbar.phpを読み込むテンプレートタグ（インクルードタグ）?>
-        <?php get_footer(); //footer.phpを読み込むテンプレートタグ（インクルードタグ）?>
-    </body>
-</html>
+            </div>
+        </div>
+    </section>
+</main>
+<?php get_sidebar(); //siderbar.phpを読み込むテンプレートタグ（インクルードタグ）?>
+<?php get_footer(); //footer.phpを読み込むテンプレートタグ（インクルードタグ）?>
