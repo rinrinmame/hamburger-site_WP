85cd744a7103c2c4d246334ef5b40e66bcdbc740
rinrinmame <rina143606@gmail.com>
2024-12-08 22:26:53 +0900

    12/8 Menuボタン不具合修正

--- js/main.js
+++ js/main.js
@@ -1,36 +1,3 @@
-/*jQuery( function( $ ) {
-	var timer = false;
-		$(window).resize(function() {
-		if (timer !== false) {
-			clearTimeout(timer);
-		}
-		timer = setTimeout(function() {
-		location.reload();
-		}, 50);
-	} );
-} );*/
-
-/*jQuery(function(){
-	var timer = false;
-	var prewidth = $(window).width();
-		$(window).resize(function() {
-		if (timer !== false) {
-			clearTimeout(timer);
-		}
-		timer = setTimeout(function() {
-			var nowWidth = $(window).width();
-				if(prewidth !== nowWidth){
-				// リロード
-					location.reload();
-				}
-		prewidth = nowWidth;
-		}, 50);
-	});
-});*/
-
-
-
-
 jQuery(document).ready( function( $ ) {
 	if (window.matchMedia( '(max-width: 1203px)' ).matches) {
 		$( ".js-menu__button" ).on( "click", function() {
