91609675d5928f5fe388cb0c3dfbe11d072fbfd5
rinrinmame <rina143606@gmail.com>
2024-12-22 17:05:11 +0900

    12/22 Menuボタンの開閉修正

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 jQuery(document).ready( function( $ ) {
-	if (window.matchMedia( '(max-width: 1203px)' ).matches) {
+	//if (window.matchMedia( '(max-width: 1203px)' ).matches) {
 		$( ".js-menu__button" ).on( "click", function() {
 			$( ".l-body" ).toggleClass( "is-fix" );
 			$( ".l-sidebar" ).toggleClass( "is-open" );
 			$( ".c-button__hamburger" ).toggleClass( "is-open" );
 		} );
-	}
-} );
+	//}
+//);
 
-jQuery(document).ready( function( $ ) {
-	if (window.matchMedia( '(max-width: 1203px)' ).matches) {
+//jQuery(document).ready( function( $ ) {
+	//if (window.matchMedia( '(max-width: 1203px)' ).matches) {
 		$( ".js-hamburger__button" ).on( "click", function() {
 			$( this ).removeClass( "is-open" );
 			$( ".l-sidebar" ).removeClass( "is-open" );
 			$( ".l-body" ).removeClass( "is-fix" );
 		} );
-	}
-} );
+	//}
+//} );
 
-jQuery(document).ready( function( $ ) {
+//jQuery(document).ready( function( $ ) {
 	$(window).resize( function() {
 		$( ".l-sidebar" ).removeClass( "is-open" );
 		$( ".l-body" ).removeClass( "is-fix" );
