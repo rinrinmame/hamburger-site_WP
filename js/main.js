6b00eba85a220f04c23928aa7f3e3710fa741658
rinrinmame <rina143606@gmail.com>
2024-12-22 17:11:21 +0900

    12/22 Menuボタンの不要なコード削除

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
 jQuery(document).ready( function( $ ) {
-	//if (window.matchMedia( '(max-width: 1203px)' ).matches) {
-		$( ".js-menu__button" ).on( "click", function() {
-			$( ".l-body" ).toggleClass( "is-fix" );
-			$( ".l-sidebar" ).toggleClass( "is-open" );
-			$( ".c-button__hamburger" ).toggleClass( "is-open" );
-		} );
-	//}
-//);
+	$( ".js-menu__button" ).on( "click", function() {
+		$( ".l-body" ).toggleClass( "is-fix" );
+		$( ".l-sidebar" ).toggleClass( "is-open" );
+		$( ".c-button__hamburger" ).toggleClass( "is-open" );
+	} );
 
-//jQuery(document).ready( function( $ ) {
-	//if (window.matchMedia( '(max-width: 1203px)' ).matches) {
-		$( ".js-hamburger__button" ).on( "click", function() {
-			$( this ).removeClass( "is-open" );
-			$( ".l-sidebar" ).removeClass( "is-open" );
-			$( ".l-body" ).removeClass( "is-fix" );
-		} );
-	//}
-//} );
-
-//jQuery(document).ready( function( $ ) {
+	$( ".js-hamburger__button" ).on( "click", function() {
+		$( this ).removeClass( "is-open" );
+		$( ".l-sidebar" ).removeClass( "is-open" );
+		$( ".l-body" ).removeClass( "is-fix" );
+	} );
+	
 	$(window).resize( function() {
 		$( ".l-sidebar" ).removeClass( "is-open" );
 		$( ".l-body" ).removeClass( "is-fix" );
